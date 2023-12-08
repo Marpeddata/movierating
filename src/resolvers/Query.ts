f46b4ea3cfa345ae9ae0d85a1a4f7da9a6ce850f
Mark <sutsko@gmail.com>
2023-12-08 10:56:07 +0100

    Fixing populate error

--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -5,14 +5,14 @@ import genreModel from "../models/genreModel";
 
 export default {
   movies: async () => {
-    const movies = await movieModel.find().populate("reviews");
+    const movies = await movieModel.find().populate(["genre", "reviews"]);
     return movies;
   },
   //the argument list takes: async (parent, args, context) args is the id, if only the args arguemtn is needed, the parent still needs to be parsed. _:any is a common way to state that the parent arguemtn is not needed, while still defining it.
   //Had issues with this syntax for a few hours since I was trying to pass async (id: String) leaving the return on the query as "undefined".
   movie: async (_: any, { id }: ObjectId) => {
     console.log(id);
-    const mov = await movieModel.findById(id).populate("reviews");
+    const mov = await movieModel.findById(id).populate(["genre", "reviews"]);
     return mov;
   },
   reviews: async () => {
