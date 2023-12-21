1bce716ce9ce426b978eb1898c03c75091266f53
Mark <sutsko@gmail.com>
2023-12-21 10:07:52 +0100

    Confirm password validation fixed

--- client/src/utility/hooks.tsx
+++ client/src/utility/hooks.tsx
@@ -1,9 +1,9 @@
-import {useState} from 'react'
+import {useState, } from 'react'
 
 
 export const useForm = (callback: any, initialState = {}) => {
   const [values, setValues] = useState(initialState)
-
+  const [confirmPassword, setConfirmPassword] = useState("");
   const onChange = (event: any) => {
 
     setValues({
@@ -11,11 +11,19 @@ export const useForm = (callback: any, initialState = {}) => {
       [event.target.name]: event.target.value
     })
     console.log(values)
+
+    if (event.target.name === "confirmPassword") {
+      setConfirmPassword(event.target.value);
+    }
   
 }
 
 const onSubmit = (event: any) => {
   event.preventDefault()
+  if (values.password !== confirmPassword) {
+    console.log("Passwords do not match");
+    return;
+  }
   callback()
 }
 
